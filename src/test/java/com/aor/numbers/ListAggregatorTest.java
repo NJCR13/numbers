33e50da76cfecaf790e3c1dec40a6d7d96c710fe
Nuno Jorge da Costa Ramos <up201906051@up.pt>
2021-11-26 17:11:42 +0000

    Initial commit

---
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -0,0 +1,56 @@
+package com.aor.numbers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListAggregatorTest {
+
+    public List<Integer> helper() {
+        List<Integer> list = Arrays.asList(1, 2, 4, 2);
+        return list;
+    }
+
+    @Test
+    public void sum() {
+        List<Integer> list = helper();
+
+        ListAggregator aggregator = new ListAggregator();
+        int sum = aggregator.sum(list);
+
+        Assertions.assertEquals(9, sum);
+    }
+
+    @Test
+    public void max() {
+        List<Integer> list = helper();
+
+        ListAggregator aggregator = new ListAggregator();
+        int max = aggregator.max(list);
+
+        Assertions.assertEquals(4, max);
+    }
+
+    @Test
+    public void min() {
+        List<Integer> list = helper();
+
+        ListAggregator aggregator = new ListAggregator();
+        int min = aggregator.min(list);
+
+        Assertions.assertEquals(1, min);
+    }
+
+    @Test
+    public void distinct() {
+        List<Integer> list = helper();
+
+        ListAggregator aggregator = new ListAggregator();
+        int distinct = aggregator.distinct(list);
+
+        Assertions.assertEquals(3, distinct);
+    }
+}
